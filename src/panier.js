a6540a3ce0e318b46416c6fd9496676c38831fd1
Evelyne <evelyne.arits162@gmail.com>
2022-03-06 21:43:17 +0000

    Display price
    Total price adjusts when element is removed

--- src/panier.js
+++ src/panier.js
@@ -1,7 +1,4 @@
-// console.log(localStorage);
-// var json = '{"name":"Zurss 50S","price":49900,"_id":"5be1ed3f1c9d44000030b061","option":"50mm 1.6"}'
-// var test = JSON.parse(json)
-// console.log(json)
+
 var myStorage = [];
 
 function allStorage() { //returns all the elements of the local storage without knowing what the key value is
@@ -21,13 +18,26 @@ function parseArray (array) {
     }
 }
 
-var storageId = [];
-var prices = [];
 
-function showShoppingCart(){
-    
-    var myShoppingCart = document.getElementById('shoppingCart');
-    
+
+let arrayIdAndPrice = [];
+let myShoppingCart = document.getElementById('shoppingCart');
+let myTotal = document.getElementById("totalPurchase"); 
+var sumPrices = 0;
+
+
+class cameraIdAndPrice {
+    constructor(id, price) {
+      this.ItemId = id;
+      this.ItemPrice = price;
+    }
+  }
+  
+  
+
+
+function showShoppingCart(){  
+
     for (var i = 0; i < myStorage.length; i++) {
         localStorage.getItem(localStorage.key(myStorage))
         var quotient = myStorage[i].price;
@@ -43,19 +53,36 @@ function showShoppingCart(){
                             <span id="quantity">${myStorage[i].option}</span>
                         </div>
                         <div class="body">
-                           
+                            
                             <div class="text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                             </div> 
-                            <button id=${myStorage[i].name} onclick="myFunction(${i})">Delete</button>  
+                            <button id=${myStorage[i].name} onclick="removeFromCart(${i})">Delete</button>  
                         </div>
                     </div>`
         
         myShoppingCart.innerHTML += template;
-        storageId.push( myStorage[i]["_id"]);
-        prices.push(parseFloat(myPrice))
+        // storageId.push( myStorage[i]["_id"]);
+        // prices.push(parseFloat(myPrice))
+
+        
+        
+        myPrice = parseFloat(myPrice)
+        var newItem = new cameraIdAndPrice(myStorage[i]["_id"], myPrice);
+        arrayIdAndPrice.push(newItem);
+
+        sumPrices += myPrice;
+        
+
     }
+    sumPrices = sumPrices.toFixed(2)
+    myTotal.innerHTML +=`<p>${sumPrices}</p>`
+
+    
 
 }
+
+
+
 
 
 
@@ -63,27 +90,110 @@ allStorage();
 parseArray(myStorage)
 showShoppingCart()
 
-function myFunction(value) {
+
+
+function showTotalPrices(prices) {
+    for (let i = 0; i < prices.length; i++ ) {
+        sumPrices += prices[i]["ItemPrice"];
+
+        var result = Number(sumPrices).toFixed(2);
+        myTotal.innerHTML= `<p>${result}<p/>`
+
+    }
+}
+
+
+
+function removeFromCart(value) {
     var myobj = document.getElementById("demo"+value);
     myobj.remove();
 
-    let key = storageId[value];
-
+    let key = arrayIdAndPrice[value]['ItemId'];
     localStorage.removeItem(key)
-}
 
-function showTotalPrices (storagePrices) {
-    let sumPrices = 0;
-    for (let i = 0; i < storagePrices.length; i++ ) {
-        sumPrices += storagePrices[i];
-    }
+    sumPrices -= arrayIdAndPrice[value]['ItemPrice']
     sumPrices = sumPrices.toFixed(2)
-    console.log(sumPrices);
-    var myTotal = document.getElementById("totalPurchase");
-    let totalDisplay = `<p>${sumPrices}</p>`
-    myTotal.innerHTML += totalDisplay;
+
+    
+
+    myTotal.innerHTML= `${sumPrices}`
+
+    delete arrayIdAndPrice[value]
+    console.log(arrayIdAndPrice)
+    
+
+    
+    
+    // if (arrayIdAndPrice.length > 1 ) {
+    //     let key = arrayIdAndPrice[value]['ItemId'];
+    //     console.log(key)
+    //     arrayIdAndPrice.splice(value, 1)
+    //     // localStorage.removeItem(key)
+    //     //showTotalPrices(arrayIdAndPrice);
+    // } else {
+    //     arrayIdAndPrice = []
+    //     // localStorage.clear()
+    //     myTotal.innerHTML = "0.00"
+    // }
+    
+
 
 }
 
-showTotalPrices(prices);
 
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+
+
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// var storageId = [];
+// var prices = [];
+
+// function showShoppingCart(){
+    
+//     var myShoppingCart = document.getElementById('shoppingCart');
+    
+//     for (var i = 0; i < myStorage.length; i++) {
+//         localStorage.getItem(localStorage.key(myStorage))
+//         var quotient = myStorage[i].price;
+//         var myPrice = quotient/1000;
+//         myPrice = myPrice.toFixed(2); 
+        
+        
+
+//     let template = `<div class="modified" id="demo${i}" >
+//                         <img src="" alt="img appareil foto" class="">
+//                         <div class="title">${myStorage[i].name} 
+//                             <span id="price">${myPrice}</span>
+//                             <span id="quantity">${myStorage[i].option}</span>
+//                         </div>
+//                         <div class="body">
+                           
+//                             <div class="text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+//                             </div> 
+//                             <button id=${myStorage[i].name} onclick="removeFromCart(${i})">Delete</button>  
+//                         </div>
+//                     </div>`
+        
+//         myShoppingCart.innerHTML += template;
+//         storageId.push( myStorage[i]["_id"]);
+//         prices.push(parseFloat(myPrice))
+//     }
+
+// }
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
